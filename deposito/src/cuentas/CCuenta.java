b837614a2a24012b022b7ad9e4985ad0cb0c0984
ckend <ckend@kendelt>
2024-02-29 01:14:48 +0100

    2do commit java doc actualizado

--- deposito/src/cuentas/CCuenta.java
+++ deposito/src/cuentas/CCuenta.java
@@ -1,4 +1,9 @@
 package cuentas;
+
+/**
+ * Clase que reprenta cuenta
+ * @author ckend
+ */
 public class CCuenta {
 
 
@@ -8,35 +13,66 @@ public class CCuenta {
     private double tipoInterés;
 
     
-    
+    /**
+     * retorna un nombre
+     * @return nombre
+     */
     public String getNombre() {
 		return nombre;
 	}
-
+    
+    /**
+     * guarda el nombre retornado
+     * @param nombre retornado
+     */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * retorna una cuenta
+	 * @return la cuenta
+	 */
 	public String getCuenta() {
 		return cuenta;
 	}
 
+	/**
+	 * Guarda la cuenta retornada
+	 * @param cuenta retornada
+	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+	/**
+	 * retorna el saldo de la cuenta
+	 * @return saldo 
+	 */
 	public double getSaldo() {
 		return saldo;
 	}
 
+	/**
+	 * guarda el saldo retornado
+	 * @param saldo retornado
+	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * retorna el tipo de interes
+	 * @return tipo de interes
+	 */
 	public double getTipoInterés() {
 		return tipoInterés;
 	}
 
+	/**
+	 * guarda el tipo de interes retornado
+	 * @param tipoInterés retornado
+	 */
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
@@ -57,6 +93,11 @@ public class CCuenta {
         return saldo;
     }
 
+    /**
+     * ingresa una cantidad que sea positiva
+     * @param cantidad a ingresar
+     * @throws Exception no puede ser negativa 
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -64,6 +105,11 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * retorna la cantidad a retirar solo si es positiva la cantidad y hay saldo disponible
+     * @param cantidad a retirar
+     * @throws Exception  la cantidad no puede ser negativa y mayor al saldo
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
